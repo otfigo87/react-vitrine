b8bb2fa0a5fcdd9e7fac0cfd9a7c1cffe879bc46
Otmane Aatik <aaotmane@gmail.com>
2023-02-10 19:13:29 -0500

    first commi & not found page

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import Project1 from "./pages/Project1";
+import Project2 from "./pages/Project2";
+import Project3 from "./pages/Project3";
+import Project4 from "./pages/Project4";
+import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
+
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Project1" element={<Project1 />} />
+        <Route path="/Project2" element={<Project2 />} />
+        <Route path="/Project3" element={<Project3 />} />
+        <Route path="/Project4" element={<Project4 />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App
---
+++ src/pages/Project4.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Project4() {
+  return (
+    <div>Project 4</div>
+  )
+}
+
+export default Project4
