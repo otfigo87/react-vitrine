0070e0d7113b3897fca480b7c25e659bbb59edf1
Otmane Aatik <aaotmane@gmail.com>
2023-02-13 23:45:31 -0500

    Home page animation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,15 +2,26 @@ import React from 'react';
 import Navigation from '../components/Navigation';
 import Mouse from '../components/Mouse';
 import SocialMedia from '../components/SocialMedia';
+import DynamicText from '../components/DynamicText';
 
 const Home = () => {
   return (
     <div>
       <Mouse />
-      <Navigation />
-      <SocialMedia />
+      <div className="home">
+        <Navigation />
+        <SocialMedia />
+        <div className="home-main">
+          <div className="main-content">
+            <h1>OA Web Development</h1>
+            <h2>
+              <DynamicText />
+            </h2>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Home
